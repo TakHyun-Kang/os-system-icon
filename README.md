f2d3d075b8d7a42b1c45a4a8615cf0e0b1d7a4fe
TakHyun-Kang <azx1054@gmail.com>
2018-08-23 18:20:36 +0900

    no message

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 작성준비중...
 
+## Installation
+
+Using npm:
+
+    $ npm i --save os-system-icon
+
+
+
 ```html
 <i class="os-icon-centos"></i>
 ```
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,41 +8,27 @@ import sass from 'gulp-sass';
 import imagemin from 'gulp-imagemin';
 
 const DIR = {
-  EX_SRC: 'example/src',
   SRC: 'src',
   IMG: 'src/imgs',
   DEST: 'dist'
 };
 
 const SRC = {
-  EX_SASS: DIR.EX_SRC + '/*.scss',
-  EX_HTML: DIR.EX_SRC + '/*.html',
   SASS: DIR.SRC + '/*.scss',
   IMAGES: DIR.IMG + '/*'
 };
 
 const DEST = {
   SASS: DIR.DEST + '/',
-  HTML: DIR.DEST + '/',
   IMAGES: DIR.DEST + '/imgs'
 };
 
-gulp.task('html', () => {
-  return gulp.src(SRC.EX_HTML)
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest(DEST.HTML))
-});
 gulp.task('sass', () => {
   return gulp.src(SRC.SASS)
     .pipe(gulp.dest(DEST.SASS));
 });
 gulp.task('sass2', () => {
   return gulp.src(SRC.SASS)
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest(DEST.SASS));
-});
-gulp.task('ex_sass', () => {
-  return gulp.src(SRC.EX_SASS)
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest(DEST.SASS));
 });
@@ -56,12 +42,11 @@ gulp.task('clean', () => {
 });
 
 gulp.task('watch', () => {
-  gulp.watch(SRC.HTML, ['html']);
   gulp.watch(SRC.SASS, ['sass2']);
   gulp.watch(SRC.EX_SASS, ['ex_sass']);
   gulp.watch(SRC.IMAGES, ['images']);
 });
 
-gulp.task('default', ['clean', 'html', 'watch', 'sass', 'sass2', 'images', 'ex_sass'], () => {
+gulp.task('default', ['clean', 'watch', 'sass', 'sass2', 'images'], () => {
   gutil.log('Gulp is running');
 });
